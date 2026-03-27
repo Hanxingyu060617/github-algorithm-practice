421cca6492018b5137559ccd4761df2932062975
Hanxingyu060617 <2084826301@qq.com>
2026-03-27 17:33:24 +0800

    增加代码的串口输出

--- ex06/ex06.ino
+++ ex06/ex06.ino
@@ -1,29 +1,32 @@
-const int ledPin1 = 2;
-const int ledPin2 = 4;
+// 作业6：警车双闪灯效
+// 2=黄灯，4=红灯
+const int ledYellow = 2;
+const int ledRed    = 4;
 
 const int freq = 5000;
-const int resolution = 8;  
+const int resolution = 8;
 
 void setup() {
   Serial.begin(115200);
-
-  // 初始化两个独立PWM通道
-  ledcAttach(ledPin1, freq, resolution);
-  ledcAttach(ledPin2, freq, resolution);
+  ledcAttach(ledYellow, freq, resolution);
+  ledcAttach(ledRed, freq, resolution);
+  Serial.println("警车双闪已启动");
 }
 
 void loop() {
-  // 反相渐变：LED1变亮，LED2变暗
+  // 黄灯渐亮，红灯渐暗
   for (int duty = 0; duty <= 255; duty++) {
-    ledcWrite(ledPin1, duty);
-    ledcWrite(ledPin2, 255 - duty);
+    ledcWrite(ledYellow, duty);
+    ledcWrite(ledRed, 255 - duty);
     delay(8);
   }
+  Serial.println("黄灯亮，红灯暗");
 
-  // 反向渐变：LED1变暗，LED2变亮
+  // 黄灯渐暗，红灯渐亮
   for (int duty = 255; duty >= 0; duty--) {
-    ledcWrite(ledPin1, duty);
-    ledcWrite(ledPin2, 255 - duty);
+    ledcWrite(ledYellow, duty);
+    ledcWrite(ledRed, 255 - duty);
     delay(8);
   }
+  Serial.println("黄灯暗，红灯亮");
 }
